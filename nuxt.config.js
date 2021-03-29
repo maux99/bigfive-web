be029646b648194f62a3b1946736209ae09cd8b5
Jaskaran Singh <jaskarannagi19@gmail.com>
2021-03-29 15:52:26 +0530

    head

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   */
   head: {
     titleTemplate: '%s - Big Five',
-    title: 'Free open-source BigFive personality traits test',
+    title: 'Big Five personality traits test',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
